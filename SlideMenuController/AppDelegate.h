d30f8ce94fff384ac8a5ee929b7bd5b34418caeb
TanguyAladenise <tanguy.aladenise@gmail.com>
2013-06-06 21:07:48 +0200

    Initial Commit

---
+++ SlideMenuController/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SlideMenuController
+//
+//  Created by Tanguy Aladenise on 6/6/13.
+//  Copyright (c) 2013 Tanguy Aladenise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SlideMenuController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SlideMenuController
+//
+//  Created by Tanguy Aladenise on 6/6/13.
+//  Copyright (c) 2013 Tanguy Aladenise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
